adaed056565899aa507ab69339df09c35ce334c5
Cory Accardo <accardo@usc.edu>
2021-11-13 17:29:22 -0800

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+    <div className="container">
+      <div className="card mx-auto" style={{backgroundColor: 'pink', width: '18rem'}}>
+        <div className="card-body">
+          <h5 className="card-title">Hello</h5>
+          <h6 className="card-subtitle mb-2 text-muted">Is it me, you're looking for?</h6>
+          <p className="card-text">I can see it in your eyes. I can see it in your smile.</p>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
