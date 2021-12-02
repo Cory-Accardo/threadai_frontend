fb6f8eeec38efc7a5d5bee9b00aac478635c35e6
Chloe Hagmann <chagmann@usc.edu>
2021-12-02 15:42:35 -0800

    homepage background color

--- src/components/Pill.js
+++ src/components/Pill.js
@@ -1,10 +1,21 @@
 import '../styles/pill.scss';
 
+const styling = {
+
+  backgroundColor: 'white',
+  fontSize: '10px',
+  
+  width: '40px',
+  border: '2px solid',
+  borderRadius: '20px'
+
+}
+
 function Pill({content, className}) { 
 
 
     return (
-      <div className={className} style="pill">
+      <div className={className} style={styling}>
           {content}
       </div>
     );
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,16 +2,20 @@ import Header from '../components/Header'
 import Form from '../components/Form'
 import Input from '../components/Input';
 import DropdownInput from '../components/DropdownInput';
-import Pill from '../components/Pill'
+import Pill from '../components/Pill';
+
 
 function Homepage() {
   return (
-    <>
+    // react fragment <> </>
+    <> 
     <Header isAuthenticated={false}/>
-    <Form hasPills={true} action="https://google.com" method='get'>
-      <Input mapKey="first_name" makesPill={true} type="text"/>
-      <DropdownInput mapKey="user_password" makesPill={true} type="text" options={[{label: 'hi'}, {label: 'bye'}]}/>
-    </Form>
+    <div style = {{backgroundColor: '#393939', height: '100%', position: 'absolute', width: '100%'}}>
+      <Form hasPills={true} action="https://google.com" method='get'>
+        <Input mapKey="first_name" makesPill={true} type="text"/>
+        <DropdownInput mapKey="user_password" makesPill={true} type="text" options={[{label: 'hi'}, {label: 'bye'}]}/>
+      </Form>
+    </div>
     </>
   );
 }
