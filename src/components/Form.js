7dfd3d3be3532b5baddf6a277a82fc6dfa149ea3
Alex Prieger <prieger@usc.edu>
2021-12-03 20:21:01 -0800

    Form now allows divs, spans, or other elements to be included, unmodified, as children.

--- src/components/Form.js
+++ src/components/Form.js
@@ -14,79 +14,117 @@ function Form({id, children, action, validation, method, onResponse}) {
     // For our purposes, performance should be irrelevant
     const [formStateArray, setFormStateArray] = useState([]);
 
-    const setInput = (index, input) => {
-        console.log('Setting state for element ' + index + ' to ' + input);
+    const setInput = (index, id, input) => {
+        console.log('Setting state for element ' + index + ' to {id: ' + id + ', state: ' + input + '}');
         const newFormState = formStateArray.slice();
-        newFormState[index] = input;
+        newFormState[index] = {
+            id: id,
+            state: input
+        };
         setFormStateArray(newFormState);
     }
 
-    const childInputs = children?.map((child, index) => {
-        switch (child.type) {
-            case DropdownInput:
-                return <DropdownInput
-                    id={child.props.id}
-                    key={child.props.id}
-                    className={child.props.className}
-                    options={child.props.options}
-                    promptText={child.props.promptText}
-                    setInput={(newInput) => setInput(index, newInput)}
-                    currentInput={formStateArray[index]}/>
-            case MultiDropdownInput:
-                return <MultiDropdownInput
-                    id={child.props.id}
-                    key={child.props.id}
-                    className={child.props.className}
-                    pillClassName={child.props.pillClassName}
-                    options={child.props.options}
-                    promptText={child.props.promptText}
-                    maxInputs={child.props.maxInputs}
-                    setInputs={(newInputs) => setInput(index, newInputs)}
-                    currentInputs={formStateArray[index]}/>
-            case Input:
-                return <Input
-                    id={child.props.id}
-                    key={child.props.id}
-                    className={child.props.className}
-                    setInput={(newInput) => setInput(index, newInput)}
-                    currentInput={formStateArray[index]}/>
-            case MultiInput:
-                return <MultiInput
-                    id={child.props.id}
-                    key={child.props.id}
-                    className={child.props.className}
-                    pillClassName={child.props.pillClassName}
-                    promptText={child.props.promptText}
-                    maxInputs={child.props.maxInputs}
-                    setInputs={(newInputs) => setInput(index, newInputs)}
-                    currentInputs={formStateArray[index]}/>
-            case SearchableDropdown:
-                return <SearchableDropdown
-                    id={child.props.id}
-                    key={child.props.id}
-                    className={child.props.className}
-                    options={child.props.options}
-                    setInput={(newInput) => setInput(index, newInput)}
-                    currentInput={formStateArray[index]}/>
-            case MultiAutocomplete:
-                return <MultiAutocomplete
-                    id={child.props.id}
-                    key={child.props.id}
-                    className={child.props.className}
-                    pillClassName={child.props.pillClassName}
-                    options={child.props.options}
-                    maxInputs={child.props.maxInputs}
-                    setInputs={(newInputs) => setInput(index, newInputs)}
-                    currentInputs={formStateArray[index]}/>
-            case 'p':
-                return child;
+    function buildFormTree(children, stateIndex) {
+        const startStateIndex = stateIndex;
+        const newChildrenTree = []
+        let childrenAsArray = [];
+        if (Array.isArray(children)) {
+            childrenAsArray = children;
+        } else if (children !== undefined) {
+            childrenAsArray[0] = children;
         }
-    });
+        for (const child of childrenAsArray) {
+            switch (child.type) {
+                case DropdownInput:
+                    stateIndex++;
+                    newChildrenTree.push(<DropdownInput
+                        id={child.props.id}
+                        key={child.props.id}
+                        className={child.props.className}
+                        options={child.props.options}
+                        promptText={child.props.promptText}
+                        /* Have to curry functions like this to capture the current value of stateIndex instead of using the final value*/
+                        setInput={((stateIndex, id) => (newInput) => setInput(stateIndex, id, newInput))(stateIndex, child.props.id)}
+                        currentInput={formStateArray[stateIndex]?.state}/>);
+                    break;
+                case MultiDropdownInput:
+                    stateIndex++;
+                    newChildrenTree.push(<MultiDropdownInput
+                        id={child.props.id}
+                        key={child.props.id}
+                        className={child.props.className}
+                        pillClassName={child.props.pillClassName}
+                        options={child.props.options}
+                        promptText={child.props.promptText}
+                        maxInputs={child.props.maxInputs}
+                        setInputs={((stateIndex, id) => (newInputs) => setInput(stateIndex, id, newInputs))(stateIndex, child.props.id)}
+                        currentInputs={formStateArray[stateIndex]?.state}/>);
+                    break;
+                case Input:
+                    stateIndex++;
+                    newChildrenTree.push(<Input
+                        id={child.props.id}
+                        key={child.props.id}
+                        className={child.props.className}
+                        setInput={((stateIndex, id) => (newInput) => setInput(stateIndex, id, newInput))(stateIndex, child.props.id)}
+                        currentInput={formStateArray[stateIndex]?.state}/>);
+                    break;
+                case MultiInput:
+                    stateIndex++;
+                    newChildrenTree.push(<MultiInput
+                        id={child.props.id}
+                        key={child.props.id}
+                        className={child.props.className}
+                        pillClassName={child.props.pillClassName}
+                        promptText={child.props.promptText}
+                        maxInputs={child.props.maxInputs}
+                        setInputs={((stateIndex, id) => (newInputs) => setInput(stateIndex, id, newInputs))(stateIndex, child.props.id)}
+                        currentInputs={formStateArray[stateIndex]?.state}/>);
+                    break;
+                case SearchableDropdown:
+                    stateIndex++;
+                    newChildrenTree.push(<SearchableDropdown
+                        id={child.props.id}
+                        key={child.props.id}
+                        className={child.props.className}
+                        options={child.props.options}
+                        setInput={((stateIndex, id) => (newInput) => setInput(stateIndex, id, newInput))(stateIndex, child.props.id)}
+                        currentInput={formStateArray[stateIndex]?.state}/>);
+                    break;
+                case MultiAutocomplete:
+                    stateIndex++;
+                    newChildrenTree.push(<MultiAutocomplete
+                        id={child.props.id}
+                        key={child.props.id}
+                        className={child.props.className}
+                        pillClassName={child.props.pillClassName}
+                        options={child.props.options}
+                        maxInputs={child.props.maxInputs}
+                        setInputs={((stateIndex, id) => (newInputs) => setInput(stateIndex, id, newInputs))(stateIndex, child.props.id)}
+                        currentInputs={formStateArray[stateIndex]?.state}/>);
+                    break;
+                case 'p':
+                    newChildrenTree.push(child);
+                    break;
+                default:
+                    const {childTree, numStateComponents} = buildFormTree(child.props.children, stateIndex);
+                    stateIndex += numStateComponents;
+                    const ChildType = child.type;
+                    newChildrenTree.push(<ChildType {...child.props}>
+                        {childTree}
+                    </ChildType>);
+                    break;
+            }
+        }
+        return {childTree: newChildrenTree, numStateComponents: stateIndex - startStateIndex};
+    }
+
+    const childInputs = buildFormTree(children, 0).childTree;
 
     function submit() {
         // Convert the state array into an object with keys named for the ids of each object, and values of the state
-        const params = formStateArray.reduce((paramMap, state, index) => {
-            paramMap[children[index].props.id] = state;
+        const params = formStateArray.reduce((paramMap, stateObj) => {
+            paramMap[stateObj.id] = stateObj.state;
             return paramMap;
         }, {})
         if (validation(params)) {
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -25,14 +25,18 @@ function Homepage() {
     <>
     <Header isAuthenticated={false}/>
     <Form hasPills={true} action="https://www.google.com" method='get' validation={validate} onResponse={handleResponse}>
-        <DropdownInput id='test_dropdown' className='test_class' options={['hi', 'bye', 'see ya']} promptText='Greeting'/>
-        <MultiDropdownInput id='test_multi_dropdown' promptText='Thing' className='test_class' pillClassName='test_class_pill' options={['1', '2', '3', '4']} maxInputs={3}/>
-        <Input id='test_input' className='test_class'/>
-        <MultiInput id='test_multi_input' promptText='Type in your three favorite animals' className='test_class' pillClassName='test_class_pill' maxInputs={3}/>
-        <p>Choose your location</p>
-        <MultiAutocomplete id='test_multi_autocomplete' className='test_class' pillClassName='test_class_pill' maxInputs={3} options={['Los Angeles', 'Absolutely nowhere', 'New York', 'Topeka']}/>
-        <p>Choose your favorite color</p>
-        <SearchableDropdown id='test_searchable_dropdown' className='test_class' options={['Red', 'Green', 'Cyan', 'Gant']}/>
+        <div>
+            <DropdownInput id='test_dropdown' className='test_class' options={['hi', 'bye', 'see ya']} promptText='Greeting'/>
+            <MultiDropdownInput id='test_multi_dropdown' promptText='Thing' className='test_class' pillClassName='test_class_pill' options={['1', '2', '3', '4']} maxInputs={3}/>
+            <span>
+                <Input id='test_input' className='test_class'/>
+                <MultiInput id='test_multi_input' promptText='Type in your three favorite animals' className='test_class' pillClassName='test_class_pill' maxInputs={3}/>
+            </span>
+            <p>Choose your location</p>
+            <MultiAutocomplete id='test_multi_autocomplete' className='test_class' pillClassName='test_class_pill' maxInputs={3} options={['Los Angeles', 'Absolutely nowhere', 'New York', 'Topeka']}/>
+            <p>Choose your favorite color</p>
+            <SearchableDropdown id='test_searchable_dropdown' className='test_class' options={['Red', 'Green', 'Cyan', 'Gant']}/>
+        </div>
     </Form>
     </>
     );
