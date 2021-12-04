4ea5518ab1adb72d0d97b00849275c1241419b11
b-go-bell <bridget.go.bell@gmail.com>
2021-12-04 14:47:55 -0800

    landing fixed, some stuff for home added

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,12 +1,10 @@
 import { useEffect } from "react";
 
-
-
 const resumeImageUrl='https://healthprofessions.missouri.edu/wp-content/uploads/sites/21/2019/11/Resume-Snip-Less-White-Space-PDF.png'
 
 const styling ={
     textAlign: 'center',
-    margin: '0px',    
+    margin: '0px',
     fontSize: '10px',
     overflow: 'hidden'
 }
@@ -21,9 +19,9 @@ function Resume({resumeObject}) {
 
     return (
         <div style={{display: 'flex', flexFlow: 'column', maxWidth:'150px'}}>
-            <p style={styling}>{author}</p>
-            <p style={styling}>{industry}</p>
-            <img width='100px' src={resumeImageUrl}/>
+            {/* <p style={styling}>{author}</p>
+            <p style={styling}>{industry}</p> */}
+            <img width='calc(100px + 2vh)' src={resumeImageUrl}/>
         </div>
     );
 }
