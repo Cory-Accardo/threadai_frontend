55942e7e61f164cccf1602fc1f885165c5a6a0ce
b-go-bell <bridget.go.bell@gmail.com>
2021-12-05 23:05:16 -0800

    basic pill styling; padding and flex needs to be added

--- src/components/Pill.js
+++ src/components/Pill.js
@@ -1,12 +1,16 @@
 // Class to display some input that the user has given from an input/dropdown box, along with the option to delete it
+import '../styles/pill.scss';
+
 function Pill({content, className, onDelete}) {
 
     return (
-        <div className={className}
-            //Style included only so pill is visible, should be deleted once properly styled
-            style={{border: 'solid 2px black'}}>
-                <button onClick={onDelete}/>
-                <p>{content}</p>
+        <div className="pill">
+            <div className="pillText">
+                {content}
+            </div>
+            <button onClick={onDelete} className="pillButton">
+                X
+            </button>
         </div>
     );
 }
